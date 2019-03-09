31312bc75a81b8896f52da205fa9ee3c2d1f5001
Mohand H <mohandhassam1@gmail.com>
2019-03-09 14:35:56 +0100

    React Router

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 
 class Connexion extends Component {
+    state = {
+        pseudo: ''
+    }
+
+    handleChange = event => {
+        const pseudo = event.target.value
+        this.setState({ pseudo })
+    }
+
     render() {
         return (
             <div className='connexionBox'>
                 <form className='connexion'>
                     <input
+                        value={this.state.pseudo}
+                        onChange={this.handleChange}
                         placeholder='Pseudo'
                         type='text'
                         required />
-                        <button>GO</button>
+                    <button>GO</button>
                 </form>
             </div>
         )
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NotFound = () => (
-    <h1 className='notFound'>Y'a rien ICI ! </h1>
+    <h2 className='notFound'>Y'a rien ICI ! </h2>
 )
 
-export default NotFound;
+export default NotFound
